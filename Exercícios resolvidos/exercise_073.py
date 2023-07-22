82ff25949c86c1fc5190ce697e9362e10e364378
Mateus Adada <mateusadada@gmail.com>
2023-07-22 11:45:51 -0300

    Update exercise_073.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_073.py
+++ Exercícios resolvidos/exercise_073.py
@@ -3,6 +3,27 @@
 # a) Os 5 primeiros times.
 # b) Os últimos 4 colocados.
 # c) Times em ordem alfabética.
-# d) Em que posição está o time da Chapecoense.
+# d) Em que posição está o time do Grêmio.
 
-# print('Bem-vindo ao programa que exibe várias informações a partir da colocação do Campeonato Brasileiro!')
+print('Bem-vindo ao programa que exibe várias informações a partir da colocação do Campeonato Brasileiro de 2023!')
+
+serie_A = ('Botafogo', 'Flamengo', 'Grêmio', 'São Paulo', 'Fluminense', 'Palmeiras', 'Bragantino', 'Atlético-PR',
+           'Fortaleza', 'Cruzeiro', 'Internacional', 'Atlético-MG', 'Cuiabá', 'Santos', 'Corinthians', 'Bahia',
+           'Goiás', 'Coritiba', 'Vasco da Gama', 'América-MG')
+
+print('\n\033[33m*** Os 5 primeiros times ***\033[m')
+for index in range(5):
+    print(f'{index + 1}º colocado: \033[33m{serie_A[index]}\033[m')
+
+print('\n\033[33m*** Os últimos 4 colocados ***\033[m')
+for index in range(16, 20):
+    print(f'{index + 1}º colocado: \033[33m{serie_A[index]}\033[m')
+
+print('\n\033[33m*** Times em ordem alfabética ***\033[m')
+for index, name in enumerate(sorted(serie_A)):
+    print(f'{index + 1}º: \033[33m{name}\033[m')
+
+print('\n\033[33m*** Em que posição está o time do Grêmio ***\033[m')
+for index, name in enumerate(serie_A):
+    if name == 'Grêmio':
+        print(f'O \033[33mGrêmio\033[m está na posição \033[33m{index + 1}\033[m')
