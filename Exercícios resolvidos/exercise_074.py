f99f035cb6968fa5caad232bfa74d7829c0223af
Mateus Adada <mateusadada@gmail.com>
2023-07-22 16:48:40 -0300

    Update exercise_074.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_074.py
+++ Exercícios resolvidos/exercise_074.py
@@ -1,4 +1,24 @@
 # Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de
 # números gerados e também indique o menor e o maior valor que estão na tupla.
 
-# print('Bem-vindo ao programa que gera cinco números aleatórios e exibe o maior e o menor!')
+from random import randint
+
+maior = menor = 0
+
+print('Bem-vindo ao programa que gera cinco números aleatórios e exibe o maior e o menor!')
+
+numeros = (randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 100))
+
+print(f'Números sorteados: \033[33m{numeros}\033[m')
+
+for index in range(5):
+    if index == 0:
+        maior = menor = numeros[index]
+    else:
+        if numeros[index] > maior:
+            maior = numeros[index]
+        if numeros[index] < menor:
+            menor = numeros[index]
+
+print(f'\nMaior: \033[33m{maior}\033[m'
+      f'\nMenor: \033[33m{menor}\033[m')
