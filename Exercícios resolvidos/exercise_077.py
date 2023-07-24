5c79b4641e92e6ff34530a2a1b2b0aa2c56e4494
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-07-24 09:39:19 -0300

    Update exercise_077.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_077.py
+++ Exercícios resolvidos/exercise_077.py
@@ -1,4 +1,16 @@
 # Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para
 # cada palavra, quais são as suas vogais.
 
-# print('Bem-vindo ao programa que exibe quais vogais tem uma palavra!')
+print('Bem-vindo ao programa que exibe quais vogais tem uma palavra!')
+
+palavras = ('crie', 'um', 'programa', 'que', 'tenha', 'uma', 'tupla', 'com', 'varias', 'palavras')
+vogais = 'aeiou'
+
+for word in palavras:
+    print(f'\033[33m"{word}"\033[m tem ', end='')
+
+    for worth in vogais:
+        if worth in word:
+            print(f'\033[33m{worth}\033[m', end=' ')
+
+    print()
