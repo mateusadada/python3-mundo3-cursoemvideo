5760e5cb191965f2f5e0a278c847cd5035f70e4c
Mateus Adada <mateusadada@gmail.com>
2023-07-23 23:25:28 -0300

    Update exercise_075.py
    Added part of the code

--- Exercícios resolvidos/exercise_075.py
+++ Exercícios resolvidos/exercise_075.py
@@ -4,3 +4,9 @@
 # C) Quais foram os números pares.
 
 # print('Bem-vindo ao programa que exibe várias informações a partir de quatro números informados!')
+
+numero1 = int(input('1º número inteiro: '))
+numero2 = int(input('2º número inteiro: '))
+numero3 = int(input('3º número inteiro: '))
+numero4 = int(input('4º número inteiro: '))
+base_numeros = (numero1, numero2, numero3, numero4)
