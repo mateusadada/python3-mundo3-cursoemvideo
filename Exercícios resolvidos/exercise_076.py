af4abc9915c417c9ca8b64c31d324171ad1a33a2
Mateus Adada <mateusadada@gmail.com>
2023-07-24 18:31:44 -0300

    Update exercise_076.py
    Replaced '-' to '=' and enumerate to len

--- Exercícios resolvidos/exercise_076.py
+++ Exercícios resolvidos/exercise_076.py
@@ -3,14 +3,20 @@
 
 print('Bem-vindo ao programa que exibe uma listagem de preços de produtos!\n')
 
-produtos_e_precos = ('Escova dental', 4.75, 'Bolacha', 2.25, 'Mentos', 10.9, 'Café', 25.65, 'Carne bovina', 33.86)
+produtos_e_precos = ('Escova dental', 4.75,
+                     'Bolacha', 2.25,
+                     'Mentos', 10.9,
+                     'Café', 25.65,
+                     'Carne bovina', 33.86)
 
-print('PRODUTO', ' ' * 17, 'PREÇO\n', end=''
-      '-' * 33)
-print()
+print('=' * 34)
+print('PRODUTO', ' ' * 17, 'PREÇO\n', end='')
+print('=' * 34)
 
-for index, worth in enumerate(produtos_e_precos):
+for index in range(0, len(produtos_e_precos)):
     if index % 2 == 0:
-        print(f'{worth:25}', end=' ')
+        print(f'{produtos_e_precos[index]:.<25}', end=' ')
     else:
-        print(f'\033[33mR$ {worth:5.2f}\033[m')
+        print(f'\033[33mR$ {produtos_e_precos[index]:>5.2f}\033[m')
+
+print('=' * 34)
