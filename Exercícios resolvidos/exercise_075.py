6ac34c9982a4a8d289b98ca85000ef2053d926fd
Mateus Adada <mateusadada@gmail.com>
2023-07-24 18:21:11 -0300

    Update exercise_075.py
    - Colocado os input dentro da tupla
    - Substituído o for para .count
    - Substituído o for para .index

--- Exercícios resolvidos/exercise_075.py
+++ Exercícios resolvidos/exercise_075.py
@@ -5,32 +5,19 @@
 
 print('Bem-vindo ao programa que exibe várias informações a partir de quatro números informados!')
 
-numero1 = int(input('1º número inteiro: '))
-numero2 = int(input('2º número inteiro: '))
-numero3 = int(input('3º número inteiro: '))
-numero4 = int(input('4º número inteiro: '))
-base_numeros = (numero1, numero2, numero3, numero4)
-contador_valor_9 = int(0)
-primeira_posicao_3 = str('')
+base_numeros = (int(input('1º número inteiro: ')),
+                int(input('2º número inteiro: ')),
+                int(input('3º número inteiro: ')),
+                int(input('4º número inteiro: ')))
 
-# contador se aparecer o valor 9
-for worth in base_numeros:
-    if worth == 9:
-        contador_valor_9 += 1
+print(f'\nQuantas vezes apareceu o valor 9: \033[33m{base_numeros.count(9)}\033[m')
 
-# primeira posição valor 3
-for index, worth in enumerate(base_numeros):
-    if worth == 3:
-        primeira_posicao_3 = f'{index + 1}º'
-        break
+if 3 in base_numeros:
+    print(f'O primeiro valor 3 está na \033[33m{base_numeros.index(3) + 1}º\033[m posição')
 else:
-    primeira_posicao_3 = 'Não foi digitado'
+    print('Não foi digitado o valor 3')
 
-print(f'\nQuantas vezes apareceu o valor 9: \033[33m{contador_valor_9}\033[m'
-      f'\nEm que posição foi digitado o primeiro valor 3: \033[33m{primeira_posicao_3}\033[m'
-      f'\nQuais foram os números pares: \033[33m', end='')
-
-# impressão números pares
-for index, worth in enumerate(base_numeros):
+print(f'Os valores pares foram: \033[33m', end='')
+for worth in base_numeros:
     if worth % 2 == 0:
         print(worth, end=' ')
