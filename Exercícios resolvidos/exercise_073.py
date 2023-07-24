e6e75cd064563ace9b07919c2fe0b51e790db34b
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-07-24 13:46:35 -0300

    Update exercise_073.py
    Adicionado como comentário outra maneira de se fazer o que está no for

--- Exercícios resolvidos/exercise_073.py
+++ Exercícios resolvidos/exercise_073.py
@@ -11,18 +11,22 @@ serie_A = ('Botafogo', 'Flamengo', 'Grêmio', 'São Paulo', 'Fluminense', 'Palme
            'Fortaleza', 'Cruzeiro', 'Internacional', 'Atlético-MG', 'Cuiabá', 'Santos', 'Corinthians', 'Bahia',
            'Goiás', 'Coritiba', 'Vasco da Gama', 'América-MG')
 
+# print(serie_A[0:5])
 print('\n\033[33m*** Os 5 primeiros times ***\033[m')
 for index in range(5):
     print(f'{index + 1}º colocado: \033[33m{serie_A[index]}\033[m')
 
+# print(serie_A[-4:])
 print('\n\033[33m*** Os últimos 4 colocados ***\033[m')
 for index in range(16, 20):
     print(f'{index + 1}º colocado: \033[33m{serie_A[index]}\033[m')
 
+# print(sorted(serie_A))
 print('\n\033[33m*** Times em ordem alfabética ***\033[m')
 for index, name in enumerate(sorted(serie_A)):
     print(f'{index + 1}º: \033[33m{name}\033[m')
 
+# print(serie_A.index('Grêmio') + 1)
 print('\n\033[33m*** Em que posição está o time do Grêmio ***\033[m')
 for index, name in enumerate(serie_A):
     if name == 'Grêmio':
