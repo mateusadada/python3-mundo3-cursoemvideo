6c1df3a5082854c9ef3c263573735a20a30a95cb
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-07-24 07:53:13 -0300

    Update exercise_076.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_076.py
+++ Exercícios resolvidos/exercise_076.py
@@ -1,4 +1,16 @@
 # Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência. No final,
 # mostre uma listagem de preços, organizando os dados em forma tabular.
 
-# print('Bem-vindo ao programa que exibe uma listagem de preços de produtos!')
+print('Bem-vindo ao programa que exibe uma listagem de preços de produtos!\n')
+
+produtos_e_precos = ('Escova dental', 4.75, 'Bolacha', 2.25, 'Mentos', 10.9, 'Café', 25.65, 'Carne bovina', 33.86)
+
+print('PRODUTO', ' ' * 17, 'PREÇO\n', end=''
+      '-' * 33)
+print()
+
+for index, worth in enumerate(produtos_e_precos):
+    if index % 2 == 0:
+        print(f'{worth:25}', end=' ')
+    else:
+        print(f'\033[33mR$ {worth:5.2f}\033[m')
