14a6655c70b7b7308c7f7cf2614363211566292e
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-07-25 07:44:51 -0300

    Update exercise_080.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_080.py
+++ Exercícios resolvidos/exercise_080.py
@@ -1,4 +1,12 @@
 # Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição
 # correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
 
-# print('Bem-vindo ao programa exibe cinco valores ordenados!')
+lista = [0, 0, 0, 0, 0]
+
+print('Bem-vindo ao programa exibe cinco valores ordenados!')
+
+for index in range(5):
+    numero = int(input(f'{index + 1}º número: '))
+    lista[index] = numero
+
+print(f'\nOs valores digitados ordenados são \033[33m{sorted(lista)}')
