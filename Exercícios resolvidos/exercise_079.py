4f7e2a72e3a1886457defe9bf76f603c13ada09e
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-07-25 07:34:54 -0300

    Update exercise_079.py
    Added the o code and a note about function

--- Exercícios resolvidos/exercise_079.py
+++ Exercícios resolvidos/exercise_079.py
@@ -2,4 +2,16 @@
 # exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem
 # crescente.
 
-# print('Bem-vindo ao programa exibe os números digitados em ordem crescente!')
+lista = []
+
+print('Bem-vindo ao programa exibe os números digitados em ordem crescente!\n')
+
+while True:
+    numero = int(input('Digite um número inteiro (999 p/ sair): '))
+
+    if numero == 999:
+        break
+    elif numero not in lista:
+        lista.append(numero)
+
+print(f'\nOs valores digitados em ordem crescente são \033[033m{sorted(lista)}')
