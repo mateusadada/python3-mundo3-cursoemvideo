d8195f29d46cd833ae3ed9bdeb66bbeeec7550f5
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-07-26 13:01:20 -0300

    Update exercise_078.py
    - Alterado a variável números para lista_números
    - Adicionado a quantidade 5 no print inicial
    - Adicionado /5 no input
    - Incluído quais as posições que estão os maiores e menores valores

--- Exercícios resolvidos/exercise_078.py
+++ Exercícios resolvidos/exercise_078.py
@@ -1,13 +1,20 @@
 # Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor
 # valor digitado e as suas respectivas posições na lista.
 
-numeros = []
+lista_numeros = []
 
-print('Bem-vindo ao programa que exibe o maior, o menor e a posição dos valores inteiros em uma lista!')
+print('Bem-vindo ao programa que exibe o maior, o menor e a posição de 5 valores inteiros em uma lista!')
 
 for index in range(5):
-    numeros.append(int(input(f'{index + 1}º número: ')))
+    lista_numeros.append(int(input(f'{index + 1}/5 número: ')))
 
-print(f'\nOs números digitados foram \033[33m{numeros}\033[m'
-      f'\nMaior: \033[33m{max(numeros)} na posição {numeros.index(max(numeros)) + 1}\033[m'
-      f'\nMenor: \033[33m{min(numeros)} na posição {numeros.index(min(numeros)) + 1}\033[m')
+print(f'\nOs números digitados foram \033[33m{lista_numeros}\033[m'
+      f'\nMaior: \033[33m{max(lista_numeros)} nas posições ', end='')
+for index, worth in enumerate(lista_numeros):
+    if worth == max(lista_numeros):
+        print(index + 1, end=' ')
+
+print(f'\n\033[mMenor: \033[33m{min(lista_numeros)} nas posições ', end='')
+for index, worth in enumerate(lista_numeros):
+    if worth == min(lista_numeros):
+        print(index + 1, end=' ')
