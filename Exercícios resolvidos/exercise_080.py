fb33de05b087c37cc985a30ca9f371845663283c
Mateus Adada <mateusadada@gmail.com>
2023-07-26 21:30:42 -0300

    Update exercise_080.py
    - Added a empty list
    - Replaced the print
    - Replaced sorted to if, else and for

--- Exercícios resolvidos/exercise_080.py
+++ Exercícios resolvidos/exercise_080.py
@@ -1,12 +1,23 @@
 # Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição
 # correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
 
-lista = [0, 0, 0, 0, 0]
+lista = []
 
-print('Bem-vindo ao programa exibe cinco valores ordenados!')
+print('Bem-vindo ao programa que recebe e exibe cinco valores ordenados!')
 
 for index in range(5):
     numero = int(input(f'{index + 1}º número: '))
-    lista[index] = numero
 
-print(f'\nOs valores digitados ordenados são \033[33m{sorted(lista)}')
+    # adiciona o 1º valor na lista
+    if len(lista) < 1:
+        lista.append(numero)
+    else:
+        # verifica se tem algum valor menor e encaixa antes
+        for index2 in range(len(lista)):
+            if numero < lista[index2]:
+                lista.insert(index2, numero)
+                break
+        else:
+            lista.append(numero)
+
+print(f'\nOs valores digitados ordenados são \033[33m{lista}')
