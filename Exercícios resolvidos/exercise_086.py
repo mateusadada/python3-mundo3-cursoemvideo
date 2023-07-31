17b7a79f508a29abeb9ff4591d7cbb7a8578f9c8
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-07-31 12:23:40 -0300

    Update exercise_086.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_086.py
+++ Exercícios resolvidos/exercise_086.py
@@ -1,4 +1,21 @@
 # Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre
 # a matriz na tela, com a formatação correta.
 
-# print('Bem-vindo ao programa que exibe uma matriz 3x3!')
+matriz = [[], [], []]
+
+print('Bem-vindo ao programa que recebe os valores e exibe uma matriz 3x3!')
+
+# preencher a matriz
+for index in range(3):
+    for index2 in range(3):
+        numero = int(input(f'Digite um número para [{index + 1}, {index2 + 1}]: '))
+        matriz[index].append(numero)
+
+# exibir a matriz
+print(f'\n\033[33m*** MATRIZ 3x3 ***\033[m', end='')
+for posicao in matriz:
+    print()
+    for index in range(3):
+        print(f'[\033[33m{posicao[index]:^5}\033[m] ', end='')
+
+print()
