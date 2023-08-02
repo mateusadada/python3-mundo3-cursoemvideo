a810eb465191701816c58d5e76ba18e0533ce62b
Mateus Adada <mateusadada@gmail.com>
2023-08-01 21:52:41 -0300

    Update exercise_089.py
    Adicionado parte do código

--- Exercícios resolvidos/exercise_089.py
+++ Exercícios resolvidos/exercise_089.py
@@ -1,7 +1,5 @@
-# Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre
+# Crie um programa que leia o nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre
 # um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.
-
-lista_alunos = []
 
 # print('Bem-vindo ao programa que gera e exibe o boletim de várias alunos!')
 
@@ -14,15 +12,22 @@ while True:
     else:
         break
 
+# cadastro
+dados_alunos = []
 for index in range(quantidade):
     # cria um espaço vazio na lista composta
-    lista_alunos.append([])
+    dados_alunos.append([])
 
     # recebe os dados e adiciona na lista composta
-    lista_alunos[index].append(str(input(f'\nNome do {index + 1}º aluno: ')).strip().upper())
-    lista_alunos[index].append(float(input('1º nota: ')))
-    lista_alunos[index].append(float(input('2º nota: ')))
-    lista_alunos[index].append(float((lista_alunos[index][1] + lista_alunos[index][2]) / 2))
+    dados_alunos[index].append(str(input(f'\nNome do {index + 1}º aluno: ')).strip().upper())
+    dados_alunos[index].append(float(input('1º nota: ')))
+    dados_alunos[index].append(float(input('2º nota: ')))
+    dados_alunos[index].append(float((dados_alunos[index][1] + dados_alunos[index][2]) / 2))
 
-    print(lista_alunos[index])
-    print(lista_alunos)
+# exibir o boletim
+print(f'\n\033[33m*** BOLETIM DOS {quantidade} ALUNOS ***\033[m'
+      f'\nCód. Nome')
+for index in range(quantidade):
+    print(f'{index + 1:<5}', end='')
+    print(f'{dados_alunos[index][0]:<25}', end='')
+    print(f'{dados_alunos[index][3]:.1f}')
