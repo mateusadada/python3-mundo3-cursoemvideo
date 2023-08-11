eecfac51650c0f91253568778441f046682cb84d
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-08-11 13:01:53 -0300

    Update exercise_091.py
    Adicionado parte do código

--- Exercícios resolvidos/exercise_091.py
+++ Exercícios resolvidos/exercise_091.py
@@ -1,4 +1,19 @@
 # Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um
 # dicionário em Python. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 
+from random import randint
+
+jogadores_final = dict()
+nome_resultado_jogadores = dict()
+
 # print('Bem-vindo ao programa que exibe quem foi o vencedor entre 4 jogadores de dados!')
+
+for index in range(1, 5):
+    # recebe o nome e o resultado do dado
+    nome_resultado_jogadores['nome'] = str(input(f'Nome do {index}º: ')).strip()
+    nome_resultado_jogadores['resultado'] = int(randint(1, 6))
+
+    # adiciona no dicionário principal
+    jogadores_final[f'jogador{index}'] = nome_resultado_jogadores.copy()
+
+print(sorted(jogadores_final.items(), key=lambda x: x[1]['resultado'], reverse=True))
