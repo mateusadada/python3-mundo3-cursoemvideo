c187e13f7821ce1803c583cd3e053e598dde804e
Mateus Adada <mateusadada@gmail.com>
2023-08-14 19:01:55 -0300

    Update exercise_091.py
    Added ranking (with sleep)

--- Exercícios resolvidos/exercise_091.py
+++ Exercícios resolvidos/exercise_091.py
@@ -2,11 +2,13 @@
 # dicionário em Python. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 
 from random import randint
+from time import sleep
 
 jogadores_final = dict()
 nome_resultado_jogadores = dict()
+contador = 1
 
-# print('Bem-vindo ao programa que exibe quem foi o vencedor entre 4 jogadores de dados!')
+print('Bem-vindo ao programa que exibe quem foi o vencedor entre 4 jogadores de dados!')
 
 for index in range(1, 5):
     # recebe o nome e o resultado do dado
@@ -16,4 +18,9 @@ for index in range(1, 5):
     # adiciona no dicionário principal
     jogadores_final[f'jogador{index}'] = nome_resultado_jogadores.copy()
 
-print(sorted(jogadores_final.items(), key=lambda x: x[1]['resultado'], reverse=True))
+# imprime a classificação
+print('\n=== RANKING DOS JOGADORES ===')
+for value in sorted(jogadores_final.items(), key=lambda x: x[1]['resultado'], reverse=True):
+    sleep(1)
+    print(f'{contador}º lugar:\033[33m {value[1]["nome"]} com {value[1]["resultado"]} pontos\033[m')
+    contador += 1
