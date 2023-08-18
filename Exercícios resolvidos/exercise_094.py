e90a36b86a80af6de0cb355cd3e3bd39f5e8b860
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-08-18 12:47:48 -0300

    Update exercise_094.py
    Removido o dicionário auxiliar

--- Exercícios resolvidos/exercise_094.py
+++ Exercícios resolvidos/exercise_094.py
@@ -5,6 +5,7 @@
 # C) Uma lista com as mulheres
 # D) Uma lista de pessoas com idade acima da média
 
+dados_da_pessoa = dict()
 lista_das_pessoas = list()
 total_idade = media_idade = 0
 
@@ -13,29 +14,20 @@ print('Bem-vindo ao programa que exibe várias informações a partir do nome, s
 # recebe os dados e organiza na lista
 while True:
     # recebe os dados
-    nome = str(input('\nDigite o nome: ')).strip().upper()
+    dados_da_pessoa.clear()
+    dados_da_pessoa['nome'] = str(input('\nDigite o nome: ')).strip().upper()
     # validação do sexo
     while True:
-        sexo = str(input('Sexo [M/F]: ')).strip().upper()[0]
-        if sexo not in 'MF':
+        dados_da_pessoa['sexo'] = str(input('Sexo [M/F]: ')).strip().upper()[0]
+        if dados_da_pessoa['sexo'] not in 'MF':
             print('\033[31mEntrada inválida! Digite "M" ou "F". Tente novamente.\033[m')
         else:
             break
-    idade = int(input('Idade: '))
+    dados_da_pessoa['idade'] = int(input('Idade: '))
+    total_idade += dados_da_pessoa['idade']
 
-    # adiciona no dicionário geral
-    dados_da_pessoa = {'nome': nome,
-                       'sexo': sexo,
-                       'idade': idade}
-
-    # faz uma cópia do dicionário geral do dicionário único
-    dados_da_pessoa[f'{len(lista_das_pessoas)}'] = dados_da_pessoa.copy()
-
-    # adiciona o dicionário único na lista
-    lista_das_pessoas.append(dados_da_pessoa[f'{len(lista_das_pessoas)}'])
-
-    # limpa o dicionário geral
-    dados_da_pessoa.clear()
+    # adiciona o dicionário na lista
+    lista_das_pessoas.append(dados_da_pessoa.copy())
 
     # validação se quer continuar
     while True:
@@ -53,8 +45,6 @@ print('\n\033[33m*** EXIBINDO OS DADOS DETALHADOS ***\033[m'
       f'\nA) Foram cadastradas \033[33m{len(lista_das_pessoas)} pessoas\033[m')
 
 # opção B
-for items in lista_das_pessoas:
-    total_idade += items['idade']
 media_idade = total_idade / len(lista_das_pessoas)
 print(f'B) A média de idade é de \033[33m{media_idade:.1f} anos\033[m')
 
