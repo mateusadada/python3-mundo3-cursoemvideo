de4e2b4850d3e518fae1413fb0528d83dfc0217d
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-08-24 12:52:50 -0300

    Created class_20_A.py
    Added the notes about class 20

---
+++ Exercícios resolvidos/class_20_A.py
@@ -0,0 +1,9 @@
+def mostrar_linha(texto):
+    print('-' * 30)
+    print(texto)
+    print('-' * 30)
+
+
+# programa principal
+mostrar_linha('      SISTEMA DE ALUNOS')
+mostrar_linha('   CADASTRO DE FUNCIONÁRIOS')
