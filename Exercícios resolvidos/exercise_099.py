7a691bfa2bced53a7ce36575cda4fe3072baddb0
Mateus Adada <mateusadada@gmail.com>
2023-08-29 19:49:21 -0300

    Update exercise_099.py
    Added the code

--- Exercícios resolvidos/exercise_099.py
+++ Exercícios resolvidos/exercise_099.py
@@ -1,4 +1,11 @@
 # Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu
 # programa tem que analisar todos os valores e dizer qual deles é o maior.
 
-# print('Bem-vindo ao programa de cálculo do maior número!')
+def maior(* numero):
+    return max(numero)
+
+
+print('Bem-vindo ao programa de cálculo do maior número!')
+
+print('\nNúmeros: 2, 5, 9, 2, 45, 7, 5, 35, 45, -87, 54'
+      f'\nMaior: \033[33m{maior(2, 5, 9, 2, 45, 7, 5, 35, 45, -87, 54)}')
