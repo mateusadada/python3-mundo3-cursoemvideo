22dab14fcbc169888d059e50943baf7d9a320ece
Mateus Adada <mateusadada@gmail.com>
2023-08-29 19:30:51 -0300

    Update exercise_096.py
    Added the code

--- Exercícios resolvidos/exercise_096.py
+++ Exercícios resolvidos/exercise_096.py
@@ -1,4 +1,14 @@
 # Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular
 # (largura e comprimento) e mostre a área do terreno.
 
-# print('Bem-vindo ao programa de cálculo da área de um terreno!')
+def calcular_area_retangulo(largura, comprimento):
+    area = largura * comprimento
+    return area
+
+
+print('Bem-vindo ao programa de cálculo da área de um terreno!')
+
+largura = float(input('Digite a largura: '))
+comprimento = float(input('Digite o comprimento: '))
+
+print(f'\nA área do terreno é \033[33m{calcular_area_retangulo(largura, comprimento):.2f} m²')
