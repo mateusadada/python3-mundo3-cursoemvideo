be8ae45dccc0d2cd99f4e900d3c1951d2b201272
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-08-30 15:16:42 -0300

    Update exercise_099.py
    Imported randint and sleep.
    
    Added a function that generates random numbers. Another that informs which is the largest number generated and another that prints a standar line.

--- Exercícios resolvidos/exercise_099.py
+++ Exercícios resolvidos/exercise_099.py
@@ -1,11 +1,37 @@
 # Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu
 # programa tem que analisar todos os valores e dizer qual deles é o maior.
 
-def maior(* numero):
-    return max(numero)
+from random import randint
+from time import sleep
+
+
+def bigger(numbers_list):
+    return max(numbers_list)
+
+
+def line():
+    print('-=' * 30)
+
+
+def random_numbers():
+    for index in range(6, 0, -1):
+        print()
+        line()
+
+        print('Analisando os valores passados...'
+              f'\nForam informados \033[33m{index}\033[m valores ao todo: \033[33m', end='')
+
+        lista = list()
+        for index2 in range(index):
+            number = randint(0, 9)
+            lista.append(number)
+            sleep(0.3)
+            print(lista[-1], end=' ')
+
+        print(f'\n\033[mO maior valor gerado foi \033[33m{bigger(lista)}\033[m.')
+        line()
 
 
 print('Bem-vindo ao programa de cálculo do maior número!')
 
-print('\nNúmeros: 2, 5, 9, 2, 45, 7, 5, 35, 45, -87 e 54'
-      f'\nMaior: \033[33m{maior(2, 5, 9, 2, 45, 7, 5, 35, 45, -87, 54)}')
+random_numbers()
