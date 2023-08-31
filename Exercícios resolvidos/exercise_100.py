e048bf72a0d87ed0411c79d1183df51052c98032
Mateus Adada <mateusadada@gmail.com>
2023-08-30 21:50:15 -0300

    Update exercise_100.py
    Added the code

--- Exercícios resolvidos/exercise_100.py
+++ Exercícios resolvidos/exercise_100.py
@@ -2,4 +2,31 @@
 # vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores
 # pares sorteados pela função anterior.
 
-# print('Bem-vindo ao programa de cálculo de 5 números aleatórios!')
+from random import randint
+from time import sleep
+
+
+def raffle():
+    print('Sorteando 5 valores e colocando na lista: \033[33m', end='')
+    for index in range(5):
+        sleep(0.5)
+        numbers.append(randint(1, 9))
+        print(numbers[-1], end=' ')
+    sleep(0.5)
+    print('PRONTO!\033[m')
+    sum_of_pairs()
+
+
+def sum_of_pairs():
+    sum_pairs = 0
+    for value in numbers:
+        if value % 2 == 0:
+            sum_pairs += value
+
+    sleep(0.5)
+    print(f'Somando os valores pares de \033[33m{numbers}\033[m temos \033[33m{sum_pairs}\033[m.')
+
+
+print('Bem-vindo ao programa de cálculo da soma de 5 números pares aleatórios!\n')
+numbers = list()
+raffle()
