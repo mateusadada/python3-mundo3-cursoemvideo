efc8c9a398a968812fe3fb03a92e8ed8a7c0fdf3
Mateus Adada <mateusadada@gmail.com>
2023-09-02 13:41:26 -0300

    Update exercise_101.py
    Changed funciton to have optional parameter.
    Message for not sending parameter.

--- Exercícios resolvidos/exercise_101.py
+++ Exercícios resolvidos/exercise_101.py
@@ -4,22 +4,24 @@
 from datetime import date
 
 
-def vote(year_of_birth):
+def vote(year_of_birth=0):
     """
     Returns a literal value indicating whether a person in voting denied, optional, or required, in addition to age.
     :param year_of_birth: The voter's year of birth.
     :return: Age and whether voting is denied, optional, or required.
     """
     age = date.today().year - year_of_birth
-    print(f'Com {age} anos: ', end='')
-    if age >= 60:
-        return '\033[33mVOTO OPCIONAL.\033[m'
-    elif 18 <= age < 60:
-        return '\033[32mVOTO OBRIGATÓRIO.\033[m'
-    elif 16 <= age < 18:
-        return '\033[33mVOTO OPCIONAL.\033[m'
+
+    if year_of_birth == 0:
+        return '\033[31mIDADE INVÁLIDA.\033[m'
     else:
-        return '\033[31mVOTO NEGADO.\033[m'
+        print(f'Com {age} anos: ', end='')
+        if 16 <= age < 18 or age >= 60:
+            return '\033[33mVOTO OPCIONAL.\033[m'
+        elif 18 <= age < 60:
+            return '\033[32mVOTO OBRIGATÓRIO.\033[m'
+        else:
+            return '\033[31mVOTO NEGADO.\033[m'
 
 
 print('Bem-vindo ao programa de verificação da situação de um eleitor!\n')
