d2e756e3df6bff736e121735220e7304725fec8d
Mateus Adada <mateusadada@gmail.com>
2023-09-05 20:43:08 -0300

    Update exercise_104.py
    Added the code

--- Exercícios resolvidos/exercise_104.py
+++ Exercícios resolvidos/exercise_104.py
@@ -2,4 +2,17 @@
 # só que fazendo a validação para aceitar apenas um valor numérico.
 # Exemplo: n = leiaInt('Digite um n: ')
 
-# print('Bem-vindo ao programa que permite apenas valores numéricos!')
+def leiaInt(text):
+    while True:
+        number = input(text).strip()
+        if number.isnumeric():
+            return number
+        else:
+            print('\033[31mEntrada inválida! Digite um número natural válido.\033[m')
+
+
+# main program
+print('Bem-vindo ao programa que permite apenas valores numéricos!')
+
+numero = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o número \033[33m{numero}\033[m.')
