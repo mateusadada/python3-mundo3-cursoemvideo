b09e3a8736f516142b5fb79a89447d1931b9ba3b
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-09-05 16:03:46 -0300

    Update exercise_102.py
    Added the code

--- Exercícios resolvidos/exercise_102.py
+++ Exercícios resolvidos/exercise_102.py
@@ -2,4 +2,28 @@
 # calcular e outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o
 # processo de cálculo do fatorial.
 
-# print('Bem-vindo ao programa de cálculo de fatorial!')
+def factorial(number, show=False):
+    """
+    Performs the factorial calculation showing or not the calculation process on the screen.
+    :param number: The number for the factorial.
+    :param show: True to show and False not to show (default).
+    :return: The factorial (with or without the calculation process).
+    """
+    sum_factorial = 1
+    while number > 0:
+        if show:
+            print(number, end=' ')
+            if number != 1:
+                print('x', end=' ')
+            else:
+                print('=', end=' ')
+        sum_factorial *= number
+        number -= 1
+
+    return sum_factorial
+
+
+print('Bem-vindo ao programa de cálculo de fatorial mostrando ou não na tela o processo!')
+
+print('-' * 30)
+print(factorial(6, True))
