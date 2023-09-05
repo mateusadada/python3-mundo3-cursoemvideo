7e6341483b2e1b333b48fd0a065dac989f3fd5dd
Mateus Adada <mateusadada@gmail.com>
2023-09-05 20:00:50 -0300

    Update exercise_103.py
    Added the code

--- Exercícios resolvidos/exercise_103.py
+++ Exercícios resolvidos/exercise_103.py
@@ -2,4 +2,25 @@
 # quantos gols ele marcou. O programa deverá conseguir mostrar a ficha do jogador, mesmo que algum dado não tenha
 # sido informado corretamente.
 
-# print('Bem-vindo ao programa que exibe várias informações de um jogador!')
+def ficha(nome='<desconhecido>', quantos_gols=0):
+    print(f'O jogador \033[33m{nome}\033[m fez \033[33m{quantos_gols}\033[m gol(s) no campeonato.')
+
+
+# main program
+print('Bem-vindo ao programa que exibe várias informações de um jogador!')
+print('-' * 30)
+
+nome_jogador = str(input('Nome do jogador: '))
+numero_de_gols = str(input('Número de gols: '))
+
+# validações e chamada da função ficha
+if nome_jogador == '':
+    if not numero_de_gols.isnumeric():
+        ficha()
+    else:
+        ficha(quantos_gols=int(numero_de_gols))
+else:
+    if not numero_de_gols.isnumeric():
+        ficha(nome_jogador)
+    else:
+        ficha(nome_jogador, int(numero_de_gols))
