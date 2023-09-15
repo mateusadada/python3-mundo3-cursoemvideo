fd5556380db3bcbe86927e2bae7a56781cc32666
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-09-15 13:00:26 -0300

    Update class_22_A.py
    Added the code

--- Exercícios resolvidos/class_22_A.py
+++ Exercícios resolvidos/class_22_A.py
@@ -1,4 +1,12 @@
 # Modularização: dividir um programa grande, aumentar a legibilidade e facilitar a manutenção.
 
-# Módulo:
-# Pacote:
+# Módulo: arquivo.py com conjunto de funções.
+# Pacote/biblioteca: conjunto de módulos; separados por assunto.
+
+from uteis import numeros
+
+num = int(input('Digite um valor: '))
+fat = numeros.fatorial(num)
+print(f'O fatorial de {num} é {fat}')
+print(f'O dobro de {num} é {numeros.dobro(num)}')
+print(f'O triplo de {num} é {numeros.triplo(num)}')
