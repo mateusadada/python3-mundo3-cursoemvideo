107a3f0f0b897783497f443aba39a96d47ffb2f7
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-09-18 12:40:52 -0300

    Update main_program.py
    Added the code

--- Exercícios resolvidos/exercise_107/main_program.py
+++ Exercícios resolvidos/exercise_107/main_program.py
@@ -1,1 +1,8 @@
-# print('Bem-vindo ao programa de vários cálculos de moeda!')
+from moeda import *
+
+print('Bem-vindo ao programa de vários cálculos de moeda a partir de um valor!')
+worth = float(input('Digite um preço: R$ '))
+
+print(f'\nA metade de R$ {worth:.2f} é R$ {metade(worth):.2f}'
+      f'\nO dobro de R$ {worth:.2f} é R$ {dobro(worth):.2f}'
+      f'\nAumentando 10% temos R$ {aumentar(worth, 10):.2f}')
