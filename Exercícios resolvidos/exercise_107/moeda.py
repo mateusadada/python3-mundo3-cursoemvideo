3285d9153a1ce4e3157c9cbb451aa17946d08c3d
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-09-18 12:47:02 -0300

    Update moeda.py
    Added the functions

--- Exercícios resolvidos/exercise_107/moeda.py
+++ Exercícios resolvidos/exercise_107/moeda.py
@@ -1,2 +1,21 @@
 # Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade(). Faça
 # também um programa que importe esse módulo e use algumas dessas funções.
+
+def aumentar(price, percentage):
+    res = price * (1 + percentage / 100)
+    return res
+
+
+def diminuir(price, percentage):
+    res = price * (1 - percentage / 100)
+    return res
+
+
+def dobro(price):
+    res = price * 2
+    return res
+
+
+def metade(price):
+    res = price / 2
+    return res
