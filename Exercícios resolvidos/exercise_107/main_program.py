707af8eb4028392fe20edf170216b12e8c6799a1
Mateus Adada <mateusadada@gmail.com>
2023-09-18 23:17:23 -0300

    Update main_program.py
    Changed '.2f' to '.1f'

--- Exercícios resolvidos/exercise_107/main_program.py
+++ Exercícios resolvidos/exercise_107/main_program.py
@@ -3,6 +3,6 @@ from moeda import *
 print('Bem-vindo ao programa de vários cálculos de moeda a partir de um valor!')
 worth = float(input('Digite um preço: R$ '))
 
-print(f'\nA metade de R$ {worth:.2f} é R$ {metade(worth):.2f}'
-      f'\nO dobro de R$ {worth:.2f} é R$ {dobro(worth):.2f}'
-      f'\nAumentando 10% temos R$ {aumentar(worth, 10):.2f}')
+print(f'\nA metade de R$ {worth:.1f} é R$ {metade(worth):.1f}'
+      f'\nO dobro de R$ {worth:.1f} é R$ {dobro(worth):.1f}'
+      f'\nAumentando 10% temos R$ {aumentar(worth, 10):.1f}')
