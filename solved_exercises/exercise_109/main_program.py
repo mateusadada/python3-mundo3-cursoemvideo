6326d659a77ee931728135d27c129d9a9a4b70ac
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-09-20 12:33:57 -0300

    Update main_program.py
    Added the code

--- solved_exercises/exercise_109/main_program.py
+++ solved_exercises/exercise_109/main_program.py
@@ -1,8 +1,9 @@
-import moeda
+from moeda import *
 
 print('Bem-vindo ao programa de vários cálculos de moeda a partir de um valor!')
 worth = float(input('Digite um preço: '))
 
-print(f'\nA metade de {moeda.moeda(worth)} é {moeda.metade(worth)}'
-      f'\nO dobro de {moeda.moeda(worth)} é {moeda.dobro(worth)}'
-      f'\nAumentando 10% temos {moeda.aumentar(worth, 10)}')
+print(f'\nA metade de {moeda(worth)} é {metade(worth, True)}'
+      f'\nO dobro de {moeda(worth)} é {dobro(worth, True)}'
+      f'\nAumentando 10% temos {aumentar(worth, 10, True)}'
+      f'\nReduzindo 13% temos {diminuir(worth, 13, True)}')
