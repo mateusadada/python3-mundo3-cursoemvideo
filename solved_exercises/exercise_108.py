025d169dbf0940d2620e6f6b09ed182a7ca4fb40
Mateus Adada <mateusadada@gmail.com>
2023-09-19 23:33:57 -0300

    Update exercise_108
    Removed exercise_108.py and replaced 'from moeda import *' to 'import moeda'

--- solved_exercises/exercise_108.py
+++
@@ -1,4 +0,0 @@
-# Adapte o código do desafio #107, criando uma função adicional chamada moeda() que consiga mostrar os números como um
-# valor monetário formatado.
-
-# print('Bem-vindo ao programa de cálculo de uma moeda!')
--- solved_exercises/exercise_108/main_program.py
+++ solved_exercises/exercise_108/main_program.py
@@ -1,8 +1,8 @@
-from moeda import *
+import moeda
 
 print('Bem-vindo ao programa de vários cálculos de moeda a partir de um valor!')
 worth = float(input('Digite um preço: '))
 
-print(f'\nA metade de {moeda(worth)} é {metade(worth)}'
-      f'\nO dobro de {moeda(worth)} é {dobro(worth)}'
-      f'\nAumentando 10% temos {aumentar(worth, 10)}')
+print(f'\nA metade de {moeda.moeda(worth)} é {moeda.metade(worth)}'
+      f'\nO dobro de {moeda.moeda(worth)} é {moeda.dobro(worth)}'
+      f'\nAumentando 10% temos {moeda.aumentar(worth, 10)}')
