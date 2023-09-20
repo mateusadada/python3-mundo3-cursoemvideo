2232eeee4af4e159b679a2174e19c958b1e9fb44
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-09-20 15:03:58 -0300

    Update moeda.py
    Added docstrings

--- solved_exercises/exercise_109/moeda.py
+++ solved_exercises/exercise_109/moeda.py
@@ -2,6 +2,13 @@
 # retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
 
 def aumentar(price, percentage, formatting=False):
+    """
+    Aumenta um preço com base em uma porcentagem passada, além de poder retornar com a formatação R$ (real).
+    :param price: O preço.
+    :param percentage: A porcentagem.
+    :param formatting: OPCIONAL - True ou False (padrão).
+    :return: O preço.
+    """
     res = price * (1 + percentage / 100)
     if formatting:
         res = moeda(res)
@@ -9,6 +16,13 @@ def aumentar(price, percentage, formatting=False):
 
 
 def diminuir(price, percentage, formatting=False):
+    """
+    Diminui um preço com base em uma porcentagem passada, além de poder retornar com a formatação R$ (real).
+    :param price: O preço.
+    :param percentage: A porcentagem.
+    :param formatting: OPCIONAL - True ou False (padrão).
+    :return: O preço.
+    """
     res = price * (1 - percentage / 100)
     if formatting:
         res = moeda(res)
@@ -16,6 +30,12 @@ def diminuir(price, percentage, formatting=False):
 
 
 def dobro(price, formatting=False):
+    """
+    Faz o cálculo do dobro de um preço, além de poder retornar com a formatação R$ (real).
+    :param price: O preço.
+    :param formatting: OPCIONAL - True ou False (padrão).
+    :return: O preço.
+    """
     res = price * 2
     if formatting:
         res = moeda(res)
@@ -23,6 +43,12 @@ def dobro(price, formatting=False):
 
 
 def metade(price, formatting=False):
+    """
+    Faz o cálculo da metade de um preço, além de poder retornar com a formatação R$ (real).
+    :param price: O preço.
+    :param formatting: OPCIONAL - True ou False (padrão).
+    :return: O preço.
+    """
     res = price / 2
     if formatting:
         res = moeda(res)
@@ -30,4 +56,10 @@ def metade(price, formatting=False):
 
 
 def moeda(price, coin='R$'):
+    """
+    Formata o preço na estrutura do real (R$) ou outro símbolo.
+    :param price: O preço.
+    :param coin: O símbolo da moeda (opcional e padrão: R$)
+    :return: O preço.
+    """
     return f'{coin}{price:.2f}'.replace('.', ',')
