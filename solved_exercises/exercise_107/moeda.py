c131045eb8536e760c4c8de1e9bbc7c0b3b2c520
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-09-20 14:49:54 -0300

    Update moeda.py
    Added docstrings

--- solved_exercises/exercise_107/moeda.py
+++ solved_exercises/exercise_107/moeda.py
@@ -2,20 +2,42 @@
 # também um programa que importe esse módulo e use algumas dessas funções.
 
 def aumentar(price, percentage):
+    """
+    Aumenta um preço com base em uma porcentagem passada.
+    :param price: O preço.
+    :param percentage: A porcentagem.
+    :return: O preço.
+    """
     res = price * (1 + percentage / 100)
     return res
 
 
 def diminuir(price, percentage):
+    """
+    Diminui um preço com base em uma porcentagem passada.
+    :param price: O preço.
+    :param percentage: A porcentagem.
+    :return: O preço.
+    """
     res = price * (1 - percentage / 100)
     return res
 
 
 def dobro(price):
+    """
+    Faz o cálculo do dobro de um preço.
+    :param price: O preço.
+    :return: O preço.
+    """
     res = price * 2
     return res
 
 
 def metade(price):
+    """
+    Faz o cálculo da metade de um preço.
+    :param price: O preço.
+    :return: O preço.
+    """
     res = price / 2
     return res
