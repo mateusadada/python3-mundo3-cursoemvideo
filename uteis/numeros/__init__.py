b3dc4955ded6127285a23fe78c6eab18b00648d2
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-09-20 14:45:05 -0300

    Update __init__.py
    Added docstrings

--- uteis/numeros/__init__.py
+++ uteis/numeros/__init__.py
@@ -1,4 +1,9 @@
 def fatorial(numero):
+    """
+    Faz o cálculo do fatorial de um número.
+    :param numero: O número.
+    :return: O fatorial.
+    """
     fat = 1
     for index in range(1, numero + 1):
         fat *= index
@@ -6,8 +11,18 @@ def fatorial(numero):
 
 
 def dobro(numero):
+    """
+    Faz o cálculo do dobro de um número.
+    :param numero: O número.
+    :return: O dobro do número.
+    """
     return numero * 2
 
 
 def triplo(numero):
+    """
+    Faz o cálculo do triplo de um número.
+    :param numero: O número.
+    :return: O triplo do número.
+    """
     return numero * 3
