69bac27aa7308bea702cb2cdc78e59ebe56e377c
Mateus Adada <mateusadada@gmail.com>
2023-09-21 18:14:44 -0300

    Update main_program.py

--- solved_exercises/exercise_111/main_program.py
+++ solved_exercises/exercise_111/main_program.py
@@ -1,5 +1,6 @@
-from solved_exercises.exercise_111.utilidadesCeV.moeda import resumo
+# from solved_exercises.exercise_111.utilidadesCeV.moeda import resumo
+# from utilidadesCeV.moeda import resumo
 
 print('Bem-vindo ao programa que exibe uma lista detalhada e organizada a partir de um valor!')
 price = float(input('Digite um preço: R$ '))
-resumo(price, 80, 35)
+# resumo(price, 80, 35)
