581f875ab0ecb14b756056475c2b99446b96ff85
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-09-21 12:42:04 -0300

    Created exercise_111
    Addes files via upload

---
+++ solved_exercises/exercise_111/main_program.py
@@ -0,0 +1,5 @@
+from solved_exercises.exercise_111.utilidadesCeV.moeda import resumo
+
+print('Bem-vindo ao programa que exibe uma lista detalhada e organizada a partir de um valor!')
+price = float(input('Digite um preço: R$ '))
+resumo(price, 80, 35)
