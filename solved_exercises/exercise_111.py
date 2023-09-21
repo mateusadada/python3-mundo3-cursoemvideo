10ed19928788c02d617544cae07d87d8987cfab1
Mateus Adada <mateusadada@gmail.com>
2023-09-20 22:56:36 -0300

    Update exercise_111.py
    Replaced code objective

--- solved_exercises/exercise_111.py
+++ solved_exercises/exercise_111.py
@@ -1,5 +1,4 @@
-# Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo chamado dado. Crie uma função chamada
-# leiaDinheiro() que seja capaz de funcionar como a função imputa(), mas com uma validação de dados para aceitar
-# apenas valores que seja monetários.
+# Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados moeda e dado. Transfira todas as
+# funções utilizadas nos desafios 107, 108 e 109 para o primeiro pacote e mantenha tudo funcionando.
 
 # print('Bem-vindo ao programa ')
