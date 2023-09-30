c08c7ee923263c6b62ad05d0eb51e8af7495be26
Mateus Adada <mateusadada@gmail.com>
2023-09-30 13:30:08 -0300

    Update exercise_114.py
    Added the code

--- solved_exercises/exercise_114.py
+++ solved_exercises/exercise_114.py
@@ -1,3 +1,12 @@
 # Crie um código em Python que teste se o site pudim está acessível pelo computador usado.
 
-# print('Bem-vindo ao programa ')
+import urllib.request
+
+print('Bem-vindo ao programa que teste se o site PUDIM está online!')
+
+try:
+    site = urllib.request.urlopen('http://pudim.com.br/')
+except urllib.error.URLError:
+    print('O site Pudim \033[31mNÃO\033[m está acessível no momento!')
+else:
+    print('\033[32mConsegui acessar o site Pudim com sucesso!\033[m')
